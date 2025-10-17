b86e627f3564bf991ad5206790db8c360b188f32
mems250005028-commits <mems250005028@iiti.ac.in>
2025-10-17 15:58:27 +0530

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+struct Item {
+    int id;
+    float cost;
+};
+
+int main() {
+    Item items[6];
+
+    for(int i = 0; i < 6; ++i) {
+        cout << "Enter ID for item " << i+1 << ": ";
+        cin >> items[i].id;
+        cout << "Enter cost for item " << i+1 << ": ";
+        cin >> items[i].cost;
+    }
+
+    cout << "\nItems with cost greater than 50.00:\n";
+    for(int i = 0; i < 6; ++i) {
+        if(items[i].cost > 50.00) {
+            cout << "Item ID: " << items[i].id << ", Cost: " << items[i].cost << endl;
+        }
+    }
+    return 0;
+}
